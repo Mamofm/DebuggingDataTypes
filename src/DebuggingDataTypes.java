52618cd2c1ae91ed03b158e82b396bbc530119d1
“Mamofm” <jmamo603@gmail.com>
2021-09-21 23:16:39 -0400

    first commit

---
+++ src/DebuggingDataTypes.java
@@ -0,0 +1,20 @@
+public class DebuggingDataTypes {
+    public static void main (String args[]) {
+        char letter = 'A';
+        short index = 1;
+        boolean answer = true;
+        String message = "I got this";
+
+        System.out.print("Char data type: ");
+        System.out.println(letter);
+
+        System.out.print("Short data type: ");
+        System.out.println(index);
+
+        System.out.print("Boolean data type: ");
+        System.out.println(answer);
+
+        System.out.print("String data type: ");
+        System.out.println(message);
+    }
+}
